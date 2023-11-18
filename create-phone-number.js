bbdeff605adc731b72325dee2fd5ef6a85fbdb61
Aaron <arnhsmith@gmail.com>
2023-11-18 15:12:02 -0500

    added create-phone-number-solution

---
+++ create-phone-number.js
@@ -0,0 +1,14 @@
+function createPhoneNumber(numbers){
+  
+
+    let string = numbers.toString();
+      
+    let cleanedString = string.replace(/[,\s[\]]/g, '');
+    
+    const edit = "(" + cleanedString.slice(0, 3) + ")" + " " + cleanedString.slice(3, 6) + "-" + cleanedString.slice(6,10);
+    
+    return edit
+    
+      
+      
+    }
